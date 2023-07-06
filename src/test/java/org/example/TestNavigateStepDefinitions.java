2830cc6366ff0fe14e4c0c84c7515fdfe4411f3e
Malusi Msomi <malusi2051@gmail.com>
2023-07-06 12:56:19 +0200

    Update TestNavigateStepDefinitions.java
    adding validation

--- src/test/java/org/example/TestNavigateStepDefinitions.java
+++ src/test/java/org/example/TestNavigateStepDefinitions.java
@@ -3,7 +3,9 @@ package org.example;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import junit.framework.Assert;
 import org.example.functions.PageFunctions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,6 +16,7 @@ public class TestNavigateStepDefinitions {
 
     private WebDriver driver;
     private PageFunctions functions;
+    private String PageTitle= "//*[@id='header']/div/div/div/div[1]/div/a/img[contains(@alt,'Website for automation practice')]";
     private String chromePath = System.getProperty("user.dir") + File.separator + "src"+ File.separator +"test"+ File.separator +"resources"+ File.separator +"drivers"+ File.separator +"chromedriver.exe";
 
     @Before
@@ -26,8 +29,8 @@ public class TestNavigateStepDefinitions {
     public void the_user_opens_the_browser() {
         driver.get("http://www.automationexercise.com/");
         driver.manage().window().maximize();
+        driver.findElement(By.xpath("//*[@id='header']//a/img[contains(@alt,'Website for automation practice')]")).isDisplayed();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
     }
 
     @After
